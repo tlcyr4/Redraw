76e9a2d35183425a8478a48a8dc03a38cd09e65f
Daniel Chae <dchaebae@nat-oitwireless-inside-vapornet100-c-15501.Princeton.EDU>
2018-03-25 10:28:11 -0400

    Locally run ReactJS frontend with Django backend

--- Redraw/frontend/config/webpack.config.dev.js
+++ Redraw/frontend/config/webpack.config.dev.js
@@ -97,7 +97,6 @@ module.exports = {
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
       new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
-      new BundleTracker({path: paths.statsRoot, filename: 'webpack-stats.dev.json'}),
     ],
   },
   module: {
@@ -245,6 +244,7 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new BundleTracker({path: paths.statsRoot, filename: 'webpack-stats.dev.json'}),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
