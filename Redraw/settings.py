eae1c2e808fa0a0b9dfab69a82a3f8eb3f78578c
Daniel Chae <dchaebae@nat-oitwireless-inside-vapornet100-c-15501.Princeton.EDU>
2018-03-31 11:58:00 -0400

    Update settings.py to accept all hosts (ALLOWED_HOSTS)

--- Redraw/settings.py
+++ Redraw/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +29,7 @@ SECRET_KEY = 'w45ac_ymm3&z#8htykcgk@-mqk#-iohojd&#5g)l#$e5@fjk$-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
