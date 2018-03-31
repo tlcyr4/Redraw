788a38ce69c9f598f4c1b84d4e90e9eef406155a
Daniel Chae <dchaebae@nat-oitwireless-inside-vapornet100-c-15501.Princeton.EDU>
2018-03-31 12:08:25 -0400

    Update settings.py

--- Redraw/settings.py
+++ Redraw/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
