b86e761da18129848e57800cdce37cd246ac897a
Daniel Chae <dchaebae@nat-oitwireless-inside-vapornet100-c-15501.Princeton.EDU>
2018-03-31 13:26:46 -0400

    Finalize herokuSetup

--- Redraw/settings.py
+++ Redraw/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -25,7 +27,7 @@ SECRET_KEY = '%@awd+z-nu9z=je!6&!mpovdnk#$nb3nchue8g!(s19nrbgv+w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['predraw.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
