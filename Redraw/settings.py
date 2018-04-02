244d4f80bf66ddeb8183b7c6b2663d368ad79445
dchaebae <dkchae@princeton.edu>
2018-04-02 13:22:01 -0400

    Successfully work with CAS Authentication

--- Redraw/settings.py
+++ Redraw/settings.py
@@ -29,6 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CAS_SERVER_URL = 'https://fed.princeton.edu/cas/'
+
 
 # Application definition
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cas_ng',
     'RedrawApp',
 ]
 
@@ -50,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'django_cas_ng.backends.CASBackend',
 ]
 
 ROOT_URLCONF = 'Redraw.urls'
@@ -108,7 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -127,3 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# for CAS authentication
+LOGIN_URL = '/login/'
