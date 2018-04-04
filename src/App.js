a5a2c5ad0103247a1950fce66968ee3c1760afb6
Chris Chu <cmechu@gmail.com>
2018-04-03 23:31:49 -0400

    have real login with authentication, but unsure how to maintain authentication between sessions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import Image1 from './Image1';
-import Login from './Login';
-/*import './App.css';*/
+import './App.css';
 import {
   Route, 
-  Switch, 
+  Switch,
   Link,
-  BrowserRouter
+  Redirect,
+  BrowserRouter,
 } from 'react-router-dom';
 
 class App extends Component {
@@ -16,15 +16,73 @@ class App extends Component {
       <BrowserRouter>
         <div className = "App">
           <Switch>
-            <Route exact path="/" component = {Login} />
-            <Route path="/map" component = {Main} /> 
-            <Route path="/image1" component = {Back} />
+            <Route path="/login" component = {Login} />
+            <PrivateRoute exact path="/" component = {Main} />
+            <PrivateRoute path="/image1" component = {Back} />
           </Switch>
         </div>
       </BrowserRouter>
     );
   }
 }
+
+class Login extends Component {
+  state = {
+    redirectToRefferrer: false
+  }; 
+
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState({ redirectToRefferrer: true});
+    });
+  };
+
+  render () {
+    return (
+      <div className="Login-page">
+        <h1 className="Welcome-page">Welcome to Redraw!</h1> 
+        <h2 className="Description">
+          Making room draw a more enjoyable experience
+        </h2>
+        <Link to="/">
+          <button className="Login-button" onClick = {this.login}>
+             Click Here to Login 
+          </button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true; 
+    setTimeout(cb, 100);
+  }, 
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+}
+
+const PrivateRoute = ({ component: Component, ...rest}) => (
+  <Route 
+    {...rest} 
+    render={props =>
+      fakeAuth.isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect 
+          to = {{
+            pathname: "/login",
+            state: {from: props.location}
+          }}
+        />
+      )
+    }
+  />
+);
 
 const Main = () => (
   <div>
@@ -34,7 +92,7 @@ const Main = () => (
 
 const Back = () => (
   <div>
-    <Link to="/map"> <Image1 /> </Link>
+    <Link to="/"> <Image1 /> </Link>
   </div>
 )
 
