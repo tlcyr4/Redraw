4c3ab084a31534bc8028b168cf1a405969adc015
dchaebae <dkchae@princeton.edu>
2018-04-11 17:52:22 -0400

    MASSIVE TEST COMMIT

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Document } from 'react-pdf';
+//import ImageMapper from 'react-image-mapper';
 import Home from './Home';
 import Image1 from './Image1';
+import Test1 from './spoon5.jpg';
 import './App.css';
 import {
   Route, 
@@ -16,10 +19,14 @@ class App extends Component {
     this.state = {
       rooms: [],
     };
+    this.roomid = 0;
+    this.imagePath = "/home";
+    this.getQuery = this.getQuery.bind(this)
   }
 
-  componentDidMount() {
-    const url = '/api/search/?building=FISHER&number=A101';
+  getQuery() {
+    const url = '/api/search/?building=WITHERSPOON&number=509';
+
     fetch(url, {credentials: "same-origin"})
       .then(response => {
         return response.json();
@@ -28,27 +35,54 @@ class App extends Component {
         this.setState({
           rooms: data
         });
-        console.log("HI");
+        this.roomid = data[0].room_id;
+        console.log(this.roomid);
         console.log(data);
       })
       .catch(error => console.log(error));
   }
 
-  render(){
+  getFloorplan() {
+    if (this.roomid === 0)
+      this.imagePath = "./spoon5.jpg";
+    else
+      this.imagePath = './spoon5.jpg';
+  }
+
+  render() {
     return (
       <BrowserRouter>
         <div className = "App">
-          <Switch>
-            <Route path="/login" component = {Login} />
-            <Route exact path="/home" component = {Main} />
-            <Route path="/image1" component = {Back} />
-          </Switch>
+          <form>
+            <input type="text"
+              placeholder="Search Room"
+              onChange={this.getQuery}
+              onSubmit={this.getFloorplan}/>
+          </form>
+          <img usemap="#test_map" src={Test1}/>
+
+          <map name="test_map">
+            <area shape="poly" coords="3671,3587,3670,4476,4202,4476,4203,4498,4460,4498,4461,4227,4405,4225,4405,3824,4461,3822,4461,3587,4051,3587,4050,3755,4044,3587" href="google.com"/>
+          </map>
         </div>
       </BrowserRouter>
-    );
+      );
   }
 }
+/* 
 
+
+*/
+
+
+
+
+
+
+/*
+<Switch>
+  <Route path={this.imagePath} component = {Main} />
+</Switch>
 class Login extends Component {
   state = {
     redirectToRefferrer: false
@@ -88,14 +122,10 @@ const fakeAuth = {
     setTimeout(cb, 100);
   }
 }
-
+*/
 
 const Main = () => (
   <div>
-    <form><input
-          type="text"
-          placeholder="Search Room"/>
-    </form>
     <Link to="/image1"> <Home /> </Link>
   </div>
 )
