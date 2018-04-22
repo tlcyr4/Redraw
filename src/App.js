09695a7dbfa7c832a790ab76401ea848b8bbbedf
dchaebae <dkchae@princeton.edu>
2018-04-21 21:00:28 -0400

    add some buttons, failing to add the events for now

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
 		};
 		this.roomid = 0;		// Room-ID, Changed on Click
 		this.floor_id = 0;	// Floor-ID for the Rooms
+		this.floorList = []; // Holds onto a list of all the floors in a building
 		this.searchLink = ""; // A string to hold the query searched
 		
 		// List of all the rooms that can be displayed, used for ImageMapper
@@ -28,6 +29,7 @@ class App extends Component {
 		
 		// Default image path is set to Wendell, but changes on click
 		this.imagePath = Wendell;
+
 		
 		// Bindings
 		this.getQuery = this.getQuery.bind(this);
@@ -54,6 +56,18 @@ class App extends Component {
 			this.getFloorplan();
 			this.floor_id = data[0].floor_id;
 	        console.log("New Rooms", this.state.rooms);
+
+	        this.floorList = [];
+	        var currFloorID = -1;
+	        // handle the number of floors there are
+	        for (var i = 0; i < data.length; i++) {
+	        	var entry = data[i];
+	        	if (currFloorID !== entry.floor_id) {
+	        		this.floorList.push(entry.level);
+	        		currFloorID = entry.floor_id;
+	        	}
+	        }
+	        console.log("All floors", this.floorList);
 	        this.forceUpdate();
 		}
       })
@@ -75,17 +89,21 @@ class App extends Component {
 	}
 
 	// Handles the click for the Polygons in the ImageMapper
-  handleClick = (obj, num, event) => {
-    var query = this.state.rooms;
-    for (var i = 0; i < query.length; i++) {
-      var iRoom = query[i];
-      // print how many sqft the room is on the console
-      if (iRoom.room_id === this.roomIDRendered[num]) {
-        console.log("Room " + iRoom.room_id + " is " + iRoom.sqft + " sqft.");
-        break;
-      }
-    }
-  }
+	handleClick = (obj, num, event) => {
+	    var query = this.state.rooms;
+	    for (var i = 0; i < query.length; i++) {
+	      var iRoom = query[i];
+	      // print how many sqft the room is on the console
+	      if (iRoom.room_id === this.roomIDRendered[num]) {
+	        console.log("Room " + iRoom.room_id + " is " + iRoom.sqft + " sqft.");
+	        break;
+	      }
+	    }
+	}
+
+	clickTest = (event) => {
+		console.log("BEEP BEEP");
+	}
 
 	// Handles the form submission by making a call to the API
 	handleSubmit(event) {
@@ -97,6 +115,8 @@ class App extends Component {
 		this.searchLink = searchData;
 		this.getQuery();
 	}
+
+	
 
 	render() {
 		// Process the JSON received from Back-End
@@ -163,6 +183,14 @@ class App extends Component {
 							onClick={(obj, num, event) => this.handleClick(obj, num, event)}
 						/>
 					</Center>
+
+
+					<ul>
+						{this.floorList && this.floorList.map(function(listValue){
+							return <li><button type="button" onClick={(event) => this.clickTest(event)}>
+							{"Floor " + parseInt(listValue)}</button></li>;
+						})}
+					</ul>
 				</div>
 			</BrowserRouter>
 		);
