92ae51f2a31eba81f78236f3b33e974dff5c0856
Kesin Ryan Dehejia <imkesin@gmail.com>
2018-04-22 20:32:27 -0400

    Made FloorPlan a Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ImageMapper from 'react-image-mapper';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faSearch from '@fortawesome/fontawesome-free-solid/faSearch'
 import Center from 'react-center';
@@ -9,6 +8,7 @@ import './App.css';
 import {
   BrowserRouter,
 } from 'react-router-dom';
+import FloorPlan from './Components/FloorPlan';
 
 // Manages the main logic of the page
 class App extends Component {
@@ -30,7 +30,6 @@ class App extends Component {
 		// Default image path is set to Wendell, but changes on click
 		this.imagePath = Wendell;
 
-		
 		// Bindings
 		this.getQuery = this.getQuery.bind(this);
 		this.getFloorplan = this.getFloorplan.bind(this);
@@ -49,7 +48,7 @@ class App extends Component {
         this.setState({
           rooms: data
         });
-        // update the room image pathing, if query
+        // Update the room image pathing, if query
 		if (Array.isArray(data) && data.length) {
 			// for now, default to the first floor
 			this.roomid = data[0].room_id;
@@ -88,72 +87,18 @@ class App extends Component {
 			.catch(error => console.log(error));
 	}
 
-	// Handles the click for the Polygons in the ImageMapper
-	handleClick = (obj, num, event) => {
-	    var query = this.state.rooms;
-	    for (var i = 0; i < query.length; i++) {
-	      var iRoom = query[i];
-	      // print how many sqft the room is on the console
-	      if (iRoom.room_id === this.roomIDRendered[num]) {
-	        console.log("Room " + iRoom.room_id + " is " + iRoom.sqft + " sqft.");
-	        break;
-	      }
-	    }
-	}
-
-	clickTest = (event) => {
-		console.log("BEEP BEEP");
-	}
-
 	// Handles the form submission by making a call to the API
 	handleSubmit(event) {
 		event.preventDefault();
 		const data = new FormData(event.target);
 		const searchData = data.get('search');
 	
-		/******************ADD ERROR PROCESSING... Finding Room that doesn't exist*****************/		
+		/* ADD ERROR PROCESSING... Finding Room that doesn't exist */		
 		this.searchLink = searchData;
 		this.getQuery();
 	}
 
-	
-
 	render() {
-		// Process the JSON received from Back-End
-		var retQuery = this.state.rooms;
-		var areaArray = [];
-		this.roomIDRendered = [];
-		var ratio = 1000.0/2550.0;
-		// Iterate through all rooms in the json file
-		for (var i = 0; i < retQuery.length; i++) {
-			var iRoom = retQuery[i];
-			// Hard-Coded to only display those on one floor
-			if (iRoom.floor_id === this.floor_id) {
-				var roomCoords = [];
-				var roomRaw = JSON.parse(iRoom.polygons);
-				
-				for (var k = 0; k < roomRaw.length; k++) {
-					var roomArray = roomRaw[k];
-					for (var j = 0; j < roomArray.length; j++) {
-						roomCoords.push(parseInt(parseInt(roomArray[j][0], 10)/4*ratio, 10));
-						roomCoords.push(parseInt(parseInt(roomArray[j][1], 10)/4*ratio, 10));
-					}
-					areaArray.push({shape: 'poly', coords: roomCoords});
-					// hold onto the order of the polygons wrt to the rooms
-					// useful for when handling click
-					this.roomIDRendered.push(iRoom.room_id);
-				}
-			}
-		}
-		// Diplays the array of polygons loaded for debugging purposes.
-		console.log("Polygons", areaArray);
-
-		// Make the MAP to be drawn in
-		var MAP = {
-			name: 'my-map',
-			areas: areaArray,
-		};
-		
 		return (
 			<BrowserRouter>
 				<div className = "App">
@@ -174,23 +119,11 @@ class App extends Component {
 						</form>
 					</div>
 					<Center>
-						<ImageMapper	
-							src={this.imagePath} 
-							map={MAP} 
-							fillColor="rgba(50,153,255,0.5)"
-							strokeColor="rgba(255, 255, 255, 0.9)"
-							width={1000} 
-							onClick={(obj, num, event) => this.handleClick(obj, num, event)}
+						<FloorPlan
+						 	rooms={this.state.rooms}
+							jpg={this.imagePath}
 						/>
 					</Center>
-
-
-					<ul>
-						{this.floorList && this.floorList.map(function(listValue){
-							return <li><button type="button" onClick={(event) => this.clickTest(event)}>
-							{"Floor " + parseInt(listValue)}</button></li>;
-						})}
-					</ul>
 				</div>
 			</BrowserRouter>
 		);
