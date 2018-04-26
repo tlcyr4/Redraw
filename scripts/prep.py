6d1b687e4c5b68bcb92028700cc2f4d34e7b4ccf
Tigar Cyr <tlcyr4@aol.com>
2018-04-26 19:39:20 -0400

    preprocessing tuning

--- scripts/prep.py
+++ scripts/prep.py
@@ -12,9 +12,14 @@ for fn in inFiles:
     print fn
     outFilename = path.join(".", "preprocessed", path.basename(fn))
     img = cv.imread(fn, cv.IMREAD_GRAYSCALE)
-    original = np.copy(img)
+    # original = np.copy(img)
     img = (255-img)
-    img = cv.threshold(img, 200, 255, cv.THRESH_BINARY)[1]
+    if "0694-00" in fn or "0696-00" in fn or "0696-01" in fn or "0696-02" in fn or "0047-00" in fn or "0060-01" in fn:
+        img = cv.threshold(img, 1, 255, cv.THRESH_BINARY)[1]
+        pass
+    else:
+        img = cv.threshold(img, 200, 255, cv.THRESH_BINARY)[1]
+    thresholded = np.copy(img)
         
 
     if len(img) > len(img[0]): 
@@ -28,18 +33,19 @@ for fn in inFiles:
         "0592-03":1,
         "0592-04":1,
         "0056":1,
-        "0696-00":5,
         "1901":1,
         "1903":1,
         "Alexander":1,
         "Pyne":1,
         "Lockhart":1,
-        "0060-0":1
+        "0060-0":1,
     }
     for nb in altBorders:
         if nb in fn:
             numBorders = altBorders[nb]
     stats[:numBorders+1,cv.CC_STAT_AREA] = 0
+
+    
     ccs = {
         "0014-01":[1,2],
         "0673-02":[1,2],
@@ -52,15 +58,29 @@ for fn in inFiles:
         "0148-01":[1],
         "0071-01":[1,2,4],
         "0694-01":[1,2],
-        "0694-00":range(1,20),
-        # "0696-01":range(1,10),
-        # "0696-02":range(1,10),
+        "0627-01":[1,2],
+        "0627-02":[1,2],
+        "0694-00":[1],
+        "0695-00":[1,3],
+        "0695-01":[1,3],
+        "0696-03":[1,3],
+        "0693-01":[1,2],
         "0153-01":[1,2,3,4,5,7,8],
         "0153-02":range(1,8),
         "0153-03":[1,2],
+        "0148-04":[1,3],
+        "0047-04":[1,2],
+        "0030-A":[1,2],
         # "0153-04":[1,2],
         "0043-05":[2],
-        "0042-04":[2]
+        "0042-04":[2],
+        "0023-01":[1,2],
+        "0040-01":[1,2,3],
+        "0603-01":[1,2,3,4],
+        "0603-02":[1,2],
+        "0153-04":[1,3],
+        "0153-A":[1,2],
+        "0619-01":[1,2]
     }
     cclist = [1]
     for cc in ccs:
@@ -83,7 +103,7 @@ for fn in inFiles:
     [cv.drawContours(mask, hull, 0, WHITE, thickness = -1, offset=origins.next()) for hull in hulls]
     mask = cv.cvtColor(mask, cv.COLOR_RGB2GRAY)
             
-    img = cv.bitwise_and((255-original), mask)
+    img = cv.bitwise_and(thresholded, mask)
 
     
 
